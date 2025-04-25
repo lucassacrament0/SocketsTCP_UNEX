da197a39c6c293a5fea4e3e413825dc9852ac196
lucassacrament0 <emersonlucas1705@gmail.com>
2025-04-24 21:19:26 -0300

    Implementação de login (usuários)

--- src/Cliente.java
+++ src/Cliente.java
@@ -5,5 +5,24 @@ import java.net.*;
 public class Cliente {
     public static void main(String[] args) throws IOException {
         Socket cliente = new Socket("localhost", 1234);
+
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+        PrintWriter saida = new PrintWriter(cliente.getOutputStream(), true);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome de usuário: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite a senha: ");
+        String senha = scanner.nextLine();
+
+        saida.println(nome);
+        saida.println(senha);
+
+        String respostaLogin = entrada.readLine();
+        System.out.println("Retorno do servidor: " + respostaLogin);
+
+        cliente.close();
     }
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -4,8 +4,41 @@ import java.net.*;
 public class Servidor {
     public static void main(String[] args) throws IOException {
         ServerSocket servidor = new ServerSocket(1234);
-        System.out.println("Aguardando conexão...");
+        System.out.println("Aguardando conexão e login...");
         Socket cliente = servidor.accept();
         System.out.println("Cliente conectado.");
+
+        //lê o que o que cliente escreve
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+        PrintWriter saida = new PrintWriter(cliente.getOutputStream(), true);
+        //envia mensagens para o cliente
+
+        String nome = entrada.readLine();
+        String senha = entrada.readLine();
+
+        //grupo de usuários cadastrados por array
+        String[][] usuarios = {
+                {"emerson", "170506"},
+                {"janaina", "310797"},
+                {"acad", "acad"}
+        };
+
+        //definir padrão para login: inválido
+        boolean loginValido = false;
+
+        //validação de ‘login’
+        for (String[] usuario : usuarios) {
+            if (usuario[0].equals(nome) && usuario[1].equals(senha)) {
+                loginValido = true;
+                saida.println("Login bem-sucedido.");
+                break;
+            }
+        else {
+        saida.println("Falha ao login.");
+        }
+
+        cliente.close();
+        servidor.close();
+        }
     }
 }
