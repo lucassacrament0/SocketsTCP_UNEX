506642ead1e8708cf72751e9461f7f0b60b0d375
lucassacrament0 <emersonlucas1705@gmail.com>
2025-04-24 21:52:01 -0300

    Criação de diretórios por usuário após login

--- src/Servidor.java
+++ src/Servidor.java
@@ -8,37 +8,54 @@ public class Servidor {
         Socket cliente = servidor.accept();
         System.out.println("Cliente conectado.");
 
-        //lê o que o que cliente escreve
+        //canais de entrada e saída na comunicação entre cliente e servidor.
         BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
         PrintWriter saida = new PrintWriter(cliente.getOutputStream(), true);
-        //envia mensagens para o cliente
 
         String nome = entrada.readLine();
         String senha = entrada.readLine();
 
-        //grupo de usuários cadastrados por array
+        //grupo de usuários cadastrados usando arrays.
         String[][] usuarios = {
                 {"emerson", "170506"},
                 {"janaina", "310797"},
                 {"acad", "acad"}
         };
 
-        //definir padrão para login: inválido
+        //padrão para login definido como inválido.
         boolean loginValido = false;
 
-        //validação de ‘login’
         for (String[] usuario : usuarios) {
             if (usuario[0].equals(nome) && usuario[1].equals(senha)) {
                 loginValido = true;
-                saida.println("Login bem-sucedido.");
                 break;
             }
-        else {
-        saida.println("Falha ao login.");
+        }
+
+        if (loginValido) {
+            saida.println("Login bem-sucedido.");
+
+            //após o login, criação de pasta do usuário e subpastas no servidor.
+            String localUsuario = "usuarios/" + nome;
+            String[] tipos = {"pdf", "jpg", "txt"};
+
+            for (String tipo : tipos) {
+                File local = new File(localUsuario + "/" + tipo);
+
+                //verificar se a pasta do usuário já existe e, se não, criá-la.
+                if (!local.exists()) {
+                    if (local.mkdirs()) {
+                        System.out.println("Local criado: " + local.getPath());
+                    } else {
+                        System.out.println("Erro ao criar local: " + local.getPath());
+                    }
+                }
+            }
+        } else {
+            saida.println("Falha ao login.");
         }
 
         cliente.close();
         servidor.close();
-        }
     }
 }
